4ccb55846f13a1305fb5f680624685c211c6b824
Camilo Henao <chenaona@everis.com>
2019-04-29 16:51:34 -0500

    Added Header section with avatar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeContainerComponent } from './containers/home-container/home-container.component';
+import { AvatarComponent } from './components/avatar/avatar.component';
+import { MenuItemComponent } from './components/menu-item/menu-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeContainerComponent,
+    AvatarComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule,
