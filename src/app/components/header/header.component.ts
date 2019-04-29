66a20060daa3c49ccfa9e53cd41807aba4811753
Camilo Henao <chenaona@everis.com>
2019-04-29 17:26:15 -0500

    Adding styles for menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  menu = [
+    {id: 0, name: "item 1"},
+    {id: 1, name: "item 2"},
+    {id: 2, name: "item 3"},
+    {id: 3, name: "item 4"}
+  ];
+
   constructor() { }
 
   ngOnInit() {
--- src/app/components/menu-item/menu-item.component.ts
+++ src/app/components/menu-item/menu-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu-item',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-item.component.css']
 })
 export class MenuItemComponent implements OnInit {
+  @Input() item: object;
 
   constructor() { }
 
